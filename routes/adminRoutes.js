1e3c5d96627b4768becf632f55430e957c8e7fcf
odilov-a <akbarjonodilov01@gmail.com>
2023-12-19 22:14:52 +0500

    full

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -16,10 +16,10 @@ const isAuthenticated = (req, res, next) => {
   if (req.session.isAuthenticated) {
     return next();
   }
-  return res.redirect("/login");
+  return res.redirect("/");
 };
 
-router.post("/login", (req, res) => {
+router.post("/", (req, res) => {
   const { username, password } = req.body;
   if (authenticateUser(username, password)) {
     req.session.isAuthenticated = true;
@@ -31,7 +31,7 @@ router.post("/login", (req, res) => {
   }
 });
 
-router.get("/login", (req, res) => {
+router.get("/", (req, res) => {
   return res.render("login", { error: null });
 });
 
@@ -40,13 +40,13 @@ router.get("/logout", isAuthenticated, (req, res) => {
     if (err) {
       console.error("Error destroying session:", err);
     }
-    return res.redirect("/login");
+    return res.redirect("/");
   });
 });
 
 router.get("/dashboard", (req, res) => {
   if (!req.session.isAuthenticated) {
-    return res.redirect("/login");
+    return res.redirect("/");
   }
   return res.render("adminDashboard");
 });

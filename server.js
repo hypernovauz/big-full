683c950c4c827da1acc928fcbe5126e5dad17bf8
odilov-a <akbarjonodilov01@gmail.com>
2023-12-24 21:16:50 +0500

    #

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+// app.set("views", path.join(__dirname, "views"));
 app.set("views", path.join(__dirname, "dist", "views"));
 app.use(express.static(path.join(__dirname, "dist")));
 app.use(bodyParser.urlencoded({ extended: true }));
